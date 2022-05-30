6dc75af4122472c9a4899d8217e4ec7e1e626feb
Ny Hasinavalona Randriantsarafara <ny.hasina@hei.school>
2022-05-30 23:32:09 +0300

    feat: implementing create and update ticket dto

--- src/ticket/dto/create-ticket.dto.ts
+++ src/ticket/dto/create-ticket.dto.ts
@@ -1,1 +1,3 @@
-export class CreateTicketDto {}
+import { Ticket } from '../entities/ticket.entity';
+
+export type CreateTicketDto = Omit<Ticket, 'id'>;
--- src/ticket/dto/update-ticket.dto.ts
+++ src/ticket/dto/update-ticket.dto.ts
@@ -1,4 +1,3 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateTicketDto } from './create-ticket.dto';
+import { Ticket } from '../entities/ticket.entity';
 
-export class UpdateTicketDto extends PartialType(CreateTicketDto) {}
+export type UpdateTicketDto = Partial<Ticket>;
