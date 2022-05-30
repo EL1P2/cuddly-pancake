1b2789f4f84b9969aaff5d790c106152d8de7cda
Ny Hasinavalona Randriantsarafara <ny.hasina@hei.school>
2022-05-30 23:21:22 +0300

    feat: implementing ticket entity

--- src/ticket/entities/ticket.entity.ts
+++ src/ticket/entities/ticket.entity.ts
@@ -1,1 +1,10 @@
-export class Ticket {}
+import { BaseModel } from '../../shared/entities/base.entity';
+
+export class Ticket extends BaseModel {
+  name: string;
+  position: string;
+  office: string;
+  age: number;
+  startDate: string;
+  salary: number;
+}
