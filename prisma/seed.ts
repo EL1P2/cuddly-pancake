bedd5781315e6e2e65d585c01fd6e9b5ecec5afd
Ny Hasinavalona Randriantsarafara <ny.hasina@hei.school>
2022-05-31 16:44:41 +0300

    chore: refactoring seed ticket method

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,22 +4,29 @@ import { faker } from '@faker-js/faker';
 const prisma = new PrismaClient();
 
 async function seedTicket() {
-  const tickets = new Array(100).fill(null).map((item, i) => ({
-    id: i,
-    name: `${faker.name.firstName()} ${faker.name.lastName()}`,
-    position: faker.word.noun(),
-    office: faker.word.noun(),
-    age: +faker.random.numeric(2),
-    startDate: faker.date.past().toISOString(),
-    salary: +faker.random.numeric(9),
-  }));
-  for (const { id, ...ticket } of tickets) {
-    await prisma.ticket.upsert({
-      where: { id },
-      create: { ...ticket },
-      update: { ...ticket },
-    });
-  }
+  return new Promise<void>(async (resolve, reject) => {
+    const tickets = new Array(100).fill(null).map((item, i) => ({
+      id: i,
+      name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+      position: faker.word.noun(),
+      office: faker.word.noun(),
+      age: +faker.random.numeric(2),
+      startDate: faker.date.past().toISOString(),
+      salary: +faker.random.numeric(9),
+    }));
+    for (const { id, ...ticket } of tickets) {
+      try {
+        await prisma.ticket.upsert({
+          where: { id },
+          create: { ...ticket },
+          update: { ...ticket },
+        });
+      } catch (e) {
+        reject(e);
+      }
+    }
+    resolve();
+  });
 }
 
 async function main() {
